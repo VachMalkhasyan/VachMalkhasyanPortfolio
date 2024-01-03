8e1097bef076955413d5dc12b3b4503854f44713
VachMalkhasyan <vachmalkhasyan@gmail.com>
2024-01-03 18:06:25 +0300

    Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,33 +1,63 @@
 <template>
-  <v-footer class="d-flex flex-column bg-green-50">
-    <div class="bg-teal d-flex w-100 align-center px-4">
-      <strong>Get connected with me on social networks!</strong>
-      <div class="d-flex flex-row ">
-          <img width="40" height="40" src="https://img.icons8.com/fluency/48/linkedin.png" alt="linkedin"/>
-          <img width="40" height="40" src="https://img.icons8.com/fluency/48/discord-logo.png" alt="discord-logo"/>
-      </div>
-      <v-spacer></v-spacer>
+  
 
-      <v-btn
-        v-for="icon in icons"
-        :key="icon"
-        class="mx-4"
-        :icon="icon"
-        variant="plain"
-        size="small"
-      ></v-btn>
+<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024  All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li class="hover:underline me-4 md:me-6" 
+        v-for="(link, index) in navLinks" :key="index"
+        @click=nav(link)
+        >
+              <span class="flex-1 ml-2 whitespace-nowrap">{{link.text}}</span>
+         </li>
+    </ul>
     </div>
+</footer>
 
-    <div class="px-4 py-2 text-center w-100 bg-green-50">
-      {{ new Date().getFullYear() }}
-    </div>
-  </v-footer>
 </template>
 
 <script>
+
+
 export default {
   name: "FooterM",
+  data() {
+    return {
+      active: 0,
+      navLinks: [
+      {
+        text: 'Home',
+        id: 'headerId'
+      },
+      {
+        text: 'Service',
+        id: 'serviceId'
+      },
+      {
+        text: 'About',
+        id: 'aboutId'
+      },
+      {
+        text: 'Contact',
+        id: 'contactId'
+      }
+    ],
+}
+},
+methods: {
+    nav(link) {
+      // Delay the scroll operation until after the component has mounted
+      this.$nextTick(() => {
+        const position = document.getElementById(link.id).offsetTop - 70
+        console.log(position, 'positionposition')
+        window.scrollTo({ top: position, behavior: 'smooth' })
+      })
+    },
+  }
 };
+
 </script>
 
 <style></style>
